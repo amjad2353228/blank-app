eba7c1d2bab299463e6724b0ed0727e7a0b3e898
amjad2353228 <1221304495@student.mmu.edu.my>
2025-11-18 14:30:11 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,16 @@
 import streamlit as st
+import clips
+import logging
+
+# Setup working environment
+logging.basicConfig(level=15, format='%(message)s')
+
+env = clips.Environment()
+router = clips.loggingRouter()
+env.add_router(router)
+
+#input 
+name = st.text input("Enter vour name")
 
 st.title("🎈 My new app")
 st.write(
