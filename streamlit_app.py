98de4b54d8ea3a6c62b29972abd41cf9a7ad28be
amjad2353228 <1221304495@student.mmu.edu.my>
2025-12-02 00:15:06 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,14 +3,30 @@ import clips
 import logging
 
 # Setup working environment
-logging.basicConfig(level=15, format='%(message)s')
+logging.basicConfig(level=15,format='%(message)s')
 
 env = clips.Environment()
-router = clips.loggingRouter()
+router = clips.LoggingRouter()
 env.add_router(router)
 
-#input 
-name = st.text input("Enter vour name")
+# input
+name = st.text_input("Enter your name")
+
+# knowledge base
+env.build('(deftemplate result (slot name))')
+# add facts to working memory
+env.assert_string(f'(result (name "{name}"))')
+# inference
+env.run()
+
+# output
+results = []
+for fact in env.facts():
+    if fact.template.name == 'result':
+        results.append(fact['name']) #Why assert the fact?
+
+st.write(results[0],"better output")
+
 
 st.title("🎈 My new app")
 st.write(
